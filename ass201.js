598d735752baee4ebc31fd5a254b03854c837a04
Nikhil10062000 <94104092+Nikhil10062000@users.noreply.github.com>
2021-12-30 11:46:10 +0530

    Add files via upload

---
+++ ass201.js
@@ -0,0 +1,19 @@
+//write a program to have 3 variables and output the lowest of all time
+
+
+var num1=prompt("Enter the number");
+    alert(num1);
+    num2=prompt("Enter the number");
+    alert(num2);
+    num3=prompt("Enter the number");
+    alert(num3);
+  
+  if (num1 < num2 && num1 < num3)
+  { console.log("num1");
+    }
+    else if (num2 < num1 && num2 < num3)
+  { console.log("num2");
+    }
+    else {
+    console.log("num3");
+}
